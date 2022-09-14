7e59f081a68a3a53cea5a7ecb46384474cbbacaa
Virgina Nita <virginanita1234@gmail.com>
2022-09-14 23:37:26 +0800

    tambahkan page payment

---
+++ src/components/AllAbout/AboutThree.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutThree = () => {
+  return <div>AboutThree</div>;
+};
+
+export default AboutThree;
---
+++ src/pages/MyCourse/MyCourse.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardMyCourse from "../../components/Card/CardMyCourse";
+
+const MyCourse = () => {
+  return (
+    <>
+      <CardMyCourse />
+    </>
+  );
+};
+
+export default MyCourse;
