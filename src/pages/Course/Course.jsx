eaed1abbd24d97ec8fae17b65b1cee40837ad7be
Virgina Nita <virginanita1234@gmail.com>
2022-09-17 19:57:04 +0800

    edit dikit

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import CardMenu from "../../components/Card/CardMenu";
+import AboutOne from "../../components/AllAbout/AboutOne";
 
 const Course = () => {
   return (
     <>
       <h1>Course</h1>
       <CardMenu />
+
+      {/* Kelebihan KSB Smart */}
+      <AboutOne />
     </>
   );
 };
